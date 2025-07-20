4df32a4d694865a607b296fcc1226fa3b4e1fb0c
Abishek <abishekkrishnat@gmail.com>
2025-07-20 19:05:21 +0530

    deploy erro solved

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Notification = require("../models/Notification");
 const User = require("../models/User");
-const Onboarding = require("../models/Onboarding");
+const Onboarding = require("../models/onboarding");
 const Job = require("../models/job");
 const Company = require("../models/Company");
 
